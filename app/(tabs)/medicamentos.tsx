46c690d98e132a6320d8757203cd0f2cb2537be4
thayanneregoo <thayanneregoo@hotmail.com>
2024-06-03 23:45:36 -0300

    adiciona quantidade por horário

--- app/(tabs)/medicamentos.tsx
+++ app/(tabs)/medicamentos.tsx
@@ -51,7 +51,8 @@ export default function Medicamentos() {
                   <ThemedText type="defaultSemiBold" >Descrição do Medicamento</ThemedText> 
 
                   <ThemedText>{tema.fields.Notes ? tema.fields.Notes:'Ainda não há descrição'}{' '}</ThemedText>
-                  <ThemedText type="defaultSemiBold">Horarios :{tema.fields.Horario}{' '}</ThemedText>             
+                  <ThemedText type="defaultSemiBold">Horarios :{tema.fields.Horario}{' '}</ThemedText>   
+                  <ThemedText type="defaultSemiBold">Quantidade por horario :{tema.fields.Quantidade}{' '}</ThemedText>              
                   </Collapsible> 
                 </ThemedView>
            
